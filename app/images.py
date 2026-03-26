5c165fc66233704abcdf15bea290090c8900d537
Sumit kumar singh <sumitkumarsingh49222@gmail.com>
2026-03-26 14:31:56 +0530

    fix imagekit config

--- app/images.py
+++ app/images.py
@@ -1,11 +1,7 @@
 from imagekitio import ImageKit
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 imagekit = ImageKit(
     private_key=os.getenv("IMAGEKIT_PRIVATE_KEY"),
-    public_key=os.getenv("IMAGEKIT_PUBLIC_KEY"),
     url_endpoint=os.getenv("IMAGEKIT_URL"),
 )
